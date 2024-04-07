6b3b4feb0121d3d7da4692eb1655069d22e20727
yeeun0118s <yeeun0118s@naver.com>
2024-04-08 02:35:14 +0900

    댓글 jwt 적용 하다가 오류

--- src/main/java/shop/mtcoding/blog/reply/ReplyService.java
+++ src/main/java/shop/mtcoding/blog/reply/ReplyService.java
@@ -19,15 +19,15 @@ public class ReplyService {
     private final ReplyJPARepository replyJPARepository;
     private final UserJPARepository userJPARepository;
 
-    @Transactional
-    public Reply 댓글쓰기(ReplyRequest.SaveDTO reqDTO, SessionUser sessionUser) {
-        Board board = boardJPARepository.findById(reqDTO.getBoardId())
-                .orElseThrow(() -> new Exception404("없는 게시글에 댓글을 작성할 수 없어요"));
-
-        Reply reply = reqDTO.toEntity(,board);
-
-        return replyJPARepository.save(reply);
-    }
+//    @Transactional
+//    public Reply 댓글쓰기(ReplyRequest.SaveDTO reqDTO, SessionUser sessionUser) {
+//        Board board = boardJPARepository.findById(reqDTO.getBoardId())
+//                .orElseThrow(() -> new Exception404("없는 게시글에 댓글을 작성할 수 없어요"));
+//
+//        Reply reply = reqDTO.toEntity(,board);
+//        replyJPARepository.save(reply);
+//        return new ReplyResponse.DTO(reply);
+//    }
     @Transactional
     public void 댓글삭제(int replyId, int sessionUserId) {
         Reply reply = replyJPARepository.findById(replyId)
